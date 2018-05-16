e410c21681b7d8bc169ca644ce50357e9cfae396
solderdev <sammer@student.tugraz.at>
2018-05-16 23:28:49 +0200

    an actual PID controller (type C)

--- app/pid.ino
+++ app/pid.ino
@@ -4,7 +4,7 @@
 #include "Arduino.h"
 
 
-#define PID_TASK_STACKSIZE  1000u  // [words]
+#define PID_TASK_STACKSIZE  2000u  // [words]
 #define PID_TASK_PRIORITY   5      // idle: 0, main-loop: 1, wifi: 2, pid: 5, sensors: 4
 
 #define PID_MIN_TEMP   10.0f  // [deg-C] minimum allowed temperature
@@ -13,8 +13,12 @@
 static float kP = 0.0f;
 static float kI = 0.0f;
 static float kD = 0.0f;
-static uint32_t ts = 0;  // [ms] update interval
+static uint32_t pid_ts = 0;  // [ms] update interval
 static float target_temp = 0.0f;  // [deg-C] target boiler temperature
+static uint8_t pid_u1 = 0;  // last output value
+static float pid_pv1 = 0.0f;  // last process value (temperature)
+static float pid_pv2 = 0.0f;  // second to last process value (temperature)
+
 static SemaphoreHandle_t pid_sem_update = NULL;
 static TimerHandle_t pid_timer_update = NULL;
 static TaskHandle_t pid_task_handle = NULL;
@@ -32,7 +36,7 @@ uint8_t PID_setup(float p, float i, float d, uint32_t ts_ms)
   kD = d;
 
   // sampling time
-  ts = ts_ms;
+  pid_ts = ts_ms;
 
   target_temp = PID_MIN_TEMP;
 
@@ -42,7 +46,7 @@ uint8_t PID_setup(float p, float i, float d, uint32_t ts_ms)
     return 1; // error
 
   // update timer - start must be called separately
-  pid_timer_update = xTimerCreate("tmr_pid", pdMS_TO_TICKS(ts_ms), pdTRUE, NULL, pid_timer_cb);
+  pid_timer_update = xTimerCreate("tmr_pid", pdMS_TO_TICKS(pid_ts), pdTRUE, NULL, pid_timer_cb);
   if (pid_timer_update == NULL)
     return 1; // error
 
@@ -55,9 +59,15 @@ uint8_t PID_setup(float p, float i, float d, uint32_t ts_ms)
 
 void PID_start(void)
 {
+  SENSORS_update();
+  pid_pv2 = SENSORS_get_temp_boiler_max();
+  pid_pv1 = SENSORS_get_temp_boiler_max();
+  pid_u1 = 0;
+  
   if (pid_timer_update != NULL)
     xTimerStart(pid_timer_update, portMAX_DELAY);
   Serial.println("PID controller ON!");
+  Serial.println(" P = " + String(kP) + " I = " + String(kI) + " D = " + String(kD));
 }
 
 void PID_stop(void)
@@ -75,6 +85,11 @@ void PID_setTargetTemp(float temp)
     target_temp = PID_MIN_TEMP;
 }
 
+float PID_getTargetTemp(void)
+{
+  return target_temp;
+}
+
 static void pid_timer_cb(TimerHandle_t pxTimer)
 {
   (void)pxTimer;
@@ -83,23 +98,79 @@ static void pid_timer_cb(TimerHandle_t pxTimer)
 
 static void pid_task(void * pvParameters)
 {
+  float pv, e;
+  int32_t u, u_limited;
+  
   (void)pvParameters;
+
+  //bool test_done = false;
   
   while(1)
   {
-    float temp = 0.0f;
+
+    // wait until the new PID-update is almost ready
+    // update temperatures shortly before
+    vTaskDelay(pdMS_TO_TICKS(pid_ts - 50));
+    SENSORS_update();
+    
     xSemaphoreTake(pid_sem_update, portMAX_DELAY);
     // Serial.println("PID running at " + String(millis()));
 
-    temp = SENSORS_get_temp_boiler_side();
+    pv = SENSORS_get_temp_boiler_max();
+    e = target_temp - pv;
 
-    // TODO
+//    // thermostat
+//    if (pv < target_temp)
+//      SSRCTRL_set_pwm_heater(100);
+//    else
+//      SSRCTRL_set_pwm_heater(0);
 
-    if (temp >= target_temp)
-      SSRCTRL_set_pwm_heater(0);
+    // PID type C
+    float p_share = -kP * (pv - pid_pv1);
+    float i_share = kI * ((float)(pid_ts)/1000.0f) * e;
+    float d_share = -(kD * (pv - 2*pid_pv1 + pid_pv2)) / ((float)(pid_ts)/1000.0f);
+    u = lroundf(pid_u1 + p_share + i_share + d_share);
+    
+//    u = lroundf(
+//        pid_u1
+//        - kP * (pv - pid_pv1)
+//        + kI * pid_ts * e 
+//        - (kD * (pv - 2*pid_pv1 + pid_pv2)) / (float)(pid_ts));
 
-    if (temp < target_temp)
-      SSRCTRL_set_pwm_heater(100);
+    u_limited = u;
+    if (u_limited < 0)
+      u_limited = 0;
+    else if (u_limited > 100)
+      u_limited = 100;
+      
+    SSRCTRL_set_pwm_heater(u_limited);
+
+    // save old values
+    pid_pv2 = pid_pv1;
+    pid_pv1 = pv;
+    pid_u1 = u_limited;
+
+    if (1)
+      Serial.println(String(millis()) + " , " + 
+                     String(target_temp) + " , " + 
+                     String(SENSORS_get_temp_boiler_side()) + " , " + 
+                     String(SENSORS_get_temp_boiler_top()) + " , " + 
+                     String(SENSORS_get_temp_brewhead()) + " , " + 
+                     String(SSRCTRL_get_pwm_heater()) + " , " + 
+                     String(SSRCTRL_get_pwm_pump()) + " , " + 
+                     String(u) + " , " + 
+                     String(p_share) + " , " + 
+                     String(i_share) + " , " + 
+                     String(d_share) );
+
+//    // step response
+//    if (!test_done && temp < 100)
+//      SSRCTRL_set_pwm_heater(100);
+//    else
+//    {
+//      SSRCTRL_set_pwm_heater(0);
+//      test_done = true;
+//    }
   }
 }
 
